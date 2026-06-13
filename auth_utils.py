bc385ff556cc312930e3bc64a82a9a0cb23ea877
Yoshitha-kondaveeti <kondaveetiyoshitha@gmail.com>
2026-06-13 10:36:29 +0530

    Update auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -1,39 +1,61 @@
 import bcrypt
 from database import conn, cursor
 
-# -----------------------------
 # Hash Password
-# -----------------------------
+
 def hash_password(password):
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
-
-# -----------------------------
 # Check Password
-# -----------------------------
+
 def check_password(password, hashed):
-    return bcrypt.checkpw(password.encode("utf-8"), hashed)
+return bcrypt.checkpw(password.encode("utf-8"), hashed)
 
-
-# -----------------------------
 # Sign Up
-# -----------------------------
+
 def signup(username, email, password):
 
-    hashed = hash_password(password)
+```
+hashed = hash_password(password)
 
-    try:
-        cursor.execute(
-            """
-            INSERT INTO users(username, email, password)
-            VALUES (?, ?, ?)
-            """,
-            (username, email, hashed)
-        )
+try:
+    cursor.execute(
+        """
+        INSERT INTO users(username, email, password)
+        VALUES (?, ?, ?)
+        """,
+        (username, email, hashed)
+    )
 
-        conn.commit()
+    conn.commit()
 
-        return True
+    return True
 
-    except Exception:
-        return False
+except Exception:
+    return False
+```
+
+# Login
+
+def login(email, password):
+
+```
+cursor.execute(
+    """
+    SELECT *
+    FROM users
+    WHERE email = ?
+    """,
+    (email,)
+)
+
+user = cursor.fetchone()
+
+if user:
+
+    if check_password(password, user[3]):
+
+        return user
+
+return None
+```
